f7846b0cbe393644b2609b2e8c47ee87176d95bd
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-01-11 23:13:37 +0100

    Binary Search Tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,29 +11,80 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
   }
 
   insert(val, currentNode=this.root) {
     // Your code here
+    let newNode = new TreeNode(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+    
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = newNode
+      } else {
+        return this.insert(val, currentNode=currentNode.right);
+      }
+    } else if(val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+      } else {
+        return this.insert(val, currentNode=currentNode.left);
+      }
+    }
   }
 
-  search(val) {
+  search(val, currentNode=this.root) {
     // Your code here
+    if (currentNode === null) return false;
+
+    if (val === currentNode.val) return true;
+
+    if (val > currentNode.val) {
+      return this.search(val, currentNode.right);
+    }
+
+    if (val < currentNode.val) {
+      return this.search(val, currentNode.left);
+    }
   }
 
 
   preOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (currentNode === null) return;
+    
+    console.log(currentNode.val); //6, 4, 2
+
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right);
   }
 
 
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (currentNode === null) return;
+
+    this.inOrderTraversal(currentNode.left);
+
+    console.log(currentNode.val);
+
+    this.inOrderTraversal(currentNode.right);
   }
 
 
   postOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (currentNode === null) return;
+
+    this.postOrderTraversal(currentNode.left);
+    this.postOrderTraversal(currentNode.right);
+
+    console.log(currentNode.val);
   }
 
     // Breadth First Traversal - Iterative
@@ -46,5 +97,23 @@ class BinarySearchTree {
     // your code here
 }
 }
+
+let bst = new BinarySearchTree();
+
+bst.insert(4);
+bst.insert(2);
+bst.insert(1);
+bst.insert(6);
+bst.insert(5);
+bst.insert(7);
+//bst.insert(12);
+
+console.log(bst);
+console.log("===============");
+
+bst.inOrderTraversal();
+
+
+
 
 module.exports = { BinarySearchTree, TreeNode };
